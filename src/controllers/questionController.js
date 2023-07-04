ee682875281ce8b322fc1896e1ba97ccecf15add
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-04 12:14:00 +0200

    feat: call Service methods inside Controller

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,20 +1,27 @@
+const questionService = require("../services/questionService");
+
 const getAllQuestions = (req, res) => {
+  const allQuestions = questionService.getAllQuestions();
   res.send("Get all Questions");
 };
 
 const getOneQuestion = (req, res) => {
+  const question = questionService.getOneQuestion();
   res.send("Get one Question");
 };
 
 const createNewQuestion = (req, res) => {
+  const createdQuestion = questionService.createNewQuestion();
   res.send("Create Question");
 };
 
 const updateOneQuestion = (req, res) => {
+  const updatedQuestion = questionService.updateOneQuestion();
   res.send("Update Question");
 };
 
 const deleteOneQuestion = (req, res) => {
+  const deletedQuestion = questionService.deleteOneQuestion();
   res.send("Delete Question");
 };
 
