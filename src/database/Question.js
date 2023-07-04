a811ee326b5f542c099235509abd54bcf50e4ba7
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-04 13:05:23 +0200

    feat: use saveToDatabase util function to sabe a new question inside db.json

--- src/database/Question.js
+++ src/database/Question.js
@@ -1,7 +1,21 @@
 const DB = require("./db.json");
-
+const { saveToDatabase } = require("./utils");
 const getAllQuestions = () => {
   return DB.questions;
 };
 
-module.exports = { getAllQuestions };
+const createNewQuestion = (newQuestion) => {
+  const isAlreadyAdded =
+    DB.questions.findIndex(
+      (question) => question.question === newQuestion.question
+    ) > -1;
+
+  if (isAlreadyAdded) {
+    return;
+  }
+
+  DB.questions.push(newQuestion);
+  saveToDatabase(DB);
+  return newQuestion;
+};
+module.exports = { getAllQuestions, createNewQuestion };
