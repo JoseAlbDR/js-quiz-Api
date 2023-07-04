ddfa9808a4c5e97473c7a4b55d117fb44065a66d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-04 11:17:33 +0200

    feat: add questionService methods

---
+++ src/services/questionService.js
@@ -0,0 +1,27 @@
+const getAllQuestions = () => {
+  return;
+};
+
+const getOneQuestion = () => {
+  return;
+};
+
+const createNewQuestion = () => {
+  return;
+};
+
+const updateOneQuestion = () => {
+  return;
+};
+
+const deleteOneQuestion = () => {
+  return;
+};
+
+module.exports = {
+  getAllQuestions,
+  getOneQuestion,
+  createNewQuestion,
+  updateOneQuestion,
+  deleteOneQuestion,
+};
