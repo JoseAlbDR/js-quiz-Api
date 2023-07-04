3a3b3ed5f4132d8637f5206b42c6531f0f34a853
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-04 12:35:15 +0200

    feat: install body-parser, use body-parser in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const v1QuestionRouter = require("./v1/routes/questionRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use("/api/v1/questions", v1QuestionRouter);
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
