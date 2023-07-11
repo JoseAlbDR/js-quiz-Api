393c8227bb6313e0520c08e10517fb1c212ed1fd
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-12 00:46:19 +0200

    feat: addn SwaggerDocs documentation url

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const v1QuestionRouter = require("./v1/routes/questionRoutes");
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,4 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use("/api/v1/questions", v1QuestionRouter);
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+  V1SwaggerDocs(app, PORT);
+});
