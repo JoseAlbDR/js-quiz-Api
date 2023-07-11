e8ef2d8d54483517ba1d38b77b1ed0bf46e6b55f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-12 00:46:51 +0200

    feat: add openapi schema

--- src/database/Question.js
+++ src/database/Question.js
@@ -28,7 +28,7 @@ const createNewQuestion = (newQuestion) => {
 
 const getOneQuestion = (questionId) => {
   const question = DB.questions.find((question) => question.id === questionId);
-  console.log(question);
+
   if (!question) return;
   return question;
 };
@@ -62,6 +62,44 @@ const deleteOneQuestion = (questionId) => {
   DB.questions.splice(indexForDelete, 1);
   saveToDatabase(DB);
 };
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Question:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           example: c72b3240-2b3e-4540-9791-09dcc9ae1c9f
+ *         question:
+ *           type: string
+ *           example: What's the output?
+ *         code:
+ *           type: string
+ *           example: console.log(typeof typeof 1);
+ *         options:
+ *           type: array
+ *           items:
+ *             type: string
+ *           example: [{"S": "number"}, {"S": "string"}, {"S": "object"},{"S": "undefined"}, ]
+ *         answer:
+ *            type: string
+ *            example: typeof 1 returns ""number"". typeof ""number"" returns ""string""
+ *         points:
+ *           type: number
+ *           example: 10
+ *         correctOption:
+ *           type: number
+ *           example: 1
+ *         createdAt:
+ *           type: string
+ *           example: 4/20/2022, 2:21:56 PM
+ *         updatedAt:
+ *           type: string
+ *           example: 4/20/2022, 2:21:56 PM
+ */
 
 module.exports = {
   getAllQuestions,
