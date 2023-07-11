ab7cfb47921524580298c8bf9c4eaab1cacc4ae8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-12 00:47:33 +0200

    feat: add openapi get all questions

--- src/v1/routes/questionRoutes.js
+++ src/v1/routes/questionRoutes.js
@@ -13,4 +13,51 @@ router.patch("/:questionId", questionController.updateOneQuestion);
 
 router.delete("/:questionId", questionController.deleteOneQuestion);
 
+/**
+ * @openapi
+ * /api/v1/questions:
+ *   get:
+ *     tags:
+ *       - Questions
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a question
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Question"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Some error message"
+ */
+
+router.get("/", questionController.getAllQuestions);
+
 module.exports = router;
