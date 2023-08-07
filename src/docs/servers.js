3699b3c99c4f7abf36caff5aae7e4c73285843f6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-07 22:48:37 +0200

    feat: add Local, testing and production server to documentation

--- src/docs/servers.js
+++ src/docs/servers.js
@@ -1,3 +1,16 @@
 module.exports = {
-  servers: [],
+  servers: [
+    {
+      url: "http://127.0.0.1:3000/api/v1/questions",
+      description: "Local Server",
+    },
+    {
+      url: "https://jsquiz.jadero.dev/api/v1/questions",
+      description: "Testing Server",
+    },
+    {
+      url: "https://jsquiz.jadero.dev/api/v1/questions",
+      description: "Production Server",
+    },
+  ],
 };
