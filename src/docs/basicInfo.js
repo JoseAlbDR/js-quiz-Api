9fa6ea157cec46cf54d3f6ef224580a3eae6ab48
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-07 22:46:05 +0200

    feat: create folder structure for swagger documentation

---
+++ src/docs/basicInfo.js
@@ -0,0 +1,13 @@
+module.exports = {
+  openapi: "3.1.0", // present supported openapi version
+  info: {
+    title: "JSQuiz API", // short title.
+    description: "API for JSQuiz Web App", //  desc.
+    version: "1.0.0", // version number
+    contact: {
+      name: "J.Alberto Delgado", // your name
+      email: "jaderodev.com", // your email
+      url: "jadelgadorobles.com", // your website
+    },
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const v1QuestionRouter = require("./v1/routes/questionRoutes");
-const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
+// const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 const dbConnect = require("./database/dbConnect");
 const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const docs = require("./docs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,10 +13,11 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api/v1/questions", v1QuestionRouter);
+app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(docs));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-  V1SwaggerDocs(app, PORT);
+  // V1SwaggerDocs(app, PORT);
 });
 
 dbConnect();
