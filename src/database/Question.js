4793c89fead15564210afcd91a1b3a13186efb68
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-08 09:08:55 +0200

    refactor: clean unused openain specification

--- src/database/Question.js
+++ src/database/Question.js
@@ -96,44 +96,6 @@ const deleteOneQuestion = (questionId) => {
   }
 };
 
-/**
- * @openapi
- * components:
- *   schemas:
- *     Question:
- *       type: object
- *       properties:
- *         id:
- *           type: string
- *           example: c72b3240-2b3e-4540-9791-09dcc9ae1c9f
- *         question:
- *           type: string
- *           example: What's the output?
- *         code:
- *           type: string
- *           example: console.log(typeof typeof 1);
- *         options:
- *           type: array
- *           items:
- *             type: string
- *           example: [{"S": "number"}, {"S": "string"}, {"S": "object"},{"S": "undefined"}, ]
- *         answer:
- *            type: string
- *            example: typeof 1 returns ""number"". typeof ""number"" returns ""string""
- *         points:
- *           type: number
- *           example: 10
- *         correctOption:
- *           type: number
- *           example: 1
- *         createdAt:
- *           type: string
- *           example: 4/20/2022, 2:21:56 PM
- *         updatedAt:
- *           type: string
- *           example: 4/20/2022, 2:21:56 PM
- */
-
 module.exports = {
   getAllQuestions,
   createNewQuestion,
